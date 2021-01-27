6529ffe864a56b947f8011a9deb36c9eb0fd2e03
Louis Coreas <l.g.coreas@gmail.com>
2021-01-27 00:13:28 +0000

    word counter changes red when word count reaches 0, also using keydown event handler to keep track of inputs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+$(document).ready(function () {
+  let spaceRemaining;
+  const maxLength = 140;
+  $("textarea").keydown(function () {
+    let comment = $.trim($("#tweet-text").val());
+    spaceRemaining = maxLength - comment.length;
+    if(spaceRemaining > 0){
+      $(this).siblings('.operators').children('.counter').text(spaceRemaining).css( 'color', '#545149');
+    }
+    /// it sets it red but it doesnt revert to black when spaceRemaining > 0
+    else{
+    $(this).siblings('.operators').children('.counter').text(spaceRemaining).css( "color", 'red');
+    }
+  });
 });
