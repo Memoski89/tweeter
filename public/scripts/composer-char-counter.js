0f62289cf67320a24bfbad52f658cac8e86fb084
Louis Coreas <l.g.coreas@gmail.com>
2021-01-27 02:31:49 +0000

    added a new section to contain tweets, tweet-container.css which style the header foorter and body of tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,12 +4,22 @@ $(document).ready(function () {
   $("textarea").keydown(function () {
     let comment = $.trim($("#tweet-text").val());
     spaceRemaining = maxLength - comment.length;
-    if(spaceRemaining > 0){
-      $(this).siblings('.operators').children('.counter').text(spaceRemaining).css( 'color', '#545149');
+    if (spaceRemaining > 0) {
+      /// if the word count is above 140  it will be at deafult color
+      // using jquery traversing to be super specific just in case we have similar code in the html
+      $(this)
+        .siblings(".operators")
+        .children(".counter")
+        .text(spaceRemaining)
+        .css("color", "#545149");
     }
-    /// it sets it red but it doesnt revert to black when spaceRemaining > 0
-    else{
-    $(this).siblings('.operators').children('.counter').text(spaceRemaining).css( "color", 'red');
+    /// if the word count goes beyond 140 the counter will turn red
+    else {
+      $(this)
+        .siblings(".operators")
+        .children(".counter")
+        .text(spaceRemaining)
+        .css("color", "red");
     }
   });
 });
